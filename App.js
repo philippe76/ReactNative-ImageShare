12d6faead04f42160476d34bdc9d3521b12d965b
Philippe Lanougadere <soufiane92230@gmail.com>
2020-12-07 11:10:05 +0100

    Create first App layout and Add image picker

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker'
 
 export default function App() {
+
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      alert('Permission to access camera roll is required!')
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+  
+    if (pickerResult.cancelled) {
+      return
+    }
+
+    setSelectedImage({localUri: pickerResult.uri})
+  }
+
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail}/>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo}/>
+      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>    
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}> Pick a photo</Text>
+      </TouchableOpacity>  
     </View>
   );
 }
@@ -18,4 +50,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15
+  },
+  logo: { 
+    width: 305, 
+    height: 159,
+    marginBottom: 10 
+  },
+  button: {
+    backgroundColor: 'dodgerblue',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain'
+  }
 });
